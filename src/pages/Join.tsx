6f999a54841d713ec43d52c82bb6e03d0572136e
longhodac <long110404@gmail.com>
2026-01-25 03:55:17 -0800

    feat: add Our Teams section with Humanoid, Internal, and Pupper teams to Join page

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -2,17 +2,17 @@ import JoinHeroSection from '../components/join/JoinHeroSection';
 import JoinCTASection from '../components/join/JoinCTASection';
 import WhyJoinCarousel from '../components/join/WhyJoinCarousel';
 import RecruitmentProcess from '../components/join/RecruitmentProcess';
-import TeamsSection from '../components/join/TeamsSection';
+import OurTeamsSection from '../components/join/OurTeamsSection';
 
 export default function Join() {
   return (
     <div className="bg-main-bg min-h-screen">
-      <div className="flex flex-col gap-12 items-center">
+      <div className="flex flex-col items-center">
         <JoinHeroSection />
         <JoinCTASection />
         <WhyJoinCarousel />
         <RecruitmentProcess />
-        <TeamsSection />
+        <OurTeamsSection />
       </div>
     </div>
   );
